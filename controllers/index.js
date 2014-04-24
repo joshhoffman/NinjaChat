7d382b3bdc3187cacfd2f1bccf832e6b7241d34f
Josh <makaze@gmail.com>
2014-04-23 19:41:41 -0500

    Got that caching working for index

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,10 @@
 
 var file = null;
 var fs = require('fs');
+var modDate = null;
+var date = new Date();
 
-exports.index = function(req, res){
+exports.indexold = function(req, res){
     if(req.isAuthenticated()) {
         res.render('index', { title: 'Dogendar', user: req.user });
     }
@@ -27,17 +29,23 @@ exports.index = function(req, res){
     }
 };
 
-exports.testindex = function(req, res) {
-    if(file === null)
-    {
-        console.log(__dirname + '/../views/Ember/index.html');
-        fs.readFile(__dirname + '/../views/Ember/index.html', 'utf8', function(err, text) {
-            file = text;
+exports.index = function(req, res) {
+
+    fs.stat(__dirname + '/../views/index.html', function(err, stats) {
+        // Cache file
+        modDate = stats.mtime;
+        if(file === null || modDate > date)
+        {
+            console.log(__dirname + '/../views/index.html');
+            fs.readFile(__dirname + '/../views/index.html', 'utf8', function(err, text) {
+                file = text;
+                res.send(file);
+            });
+        }
+        else
+        {
+            console.log('cached');
             res.send(file);
-        });
-    }
-    else
-    {
-        res.send(file);
-    }
+        }
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@ var index = require('../controllers/index.js');
 
 module.exports = function(app) {
     app.get('/', index.index);
-    app.get('/test', index.testindex);
+    app.get('/test', index.indexold);
 };
