851d3f359146df3042b8f25c5cae62b948fb62ea
josh <makaze@gmail.com>
2014-04-24 13:48:10 +0000

    Cleanup

--- models/User.js
+++ models/User.js
@@ -6,19 +6,7 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
     username: String,
     password: String,
-    displayname: String,
-    reminders: [{
-        event: String,
-        lastRun: Date,
-        frequency: Number,
-        category: [String]
-    }],
-    schedule: [{
-        day: [String],
-        time: String,
-        event: String,
-        category: [String]
-    }]
+    displayname: String
 });
 
 exports.userModel = mongoose.model("UserSchema", userSchema);
