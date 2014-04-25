c5295b0037de2f2c3aa9b17bcbce06839642918c
josh <makaze@gmail.com>
2014-04-25 14:34:47 +0000

    Add under construction page/server. Need to updatte for express 4

---
+++ OneFFUnderConstruction/routes/index.js
@@ -0,0 +1,10 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  //res.render('index', { title: 'Express' });
+    //res.sendfile('/../public/index.html');
+    res.render('index.html');
+};
